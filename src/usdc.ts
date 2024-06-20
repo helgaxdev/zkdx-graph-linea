24f6dfd120ed84dd1c4585c179b8fe085efcab71
helga <helgaschneider823@gmail.com>
2024-06-20 21:07:54 +0800

    v0.0.8 ignore the zusd pool for now

--- src/usdc.ts
+++ src/usdc.ts
@@ -4,7 +4,7 @@ import {TokenBalance, TokenBalanceShot} from "../generated/schema";
 let USDC = "0x176211869ca2b568f2a7d4ee941e073a821ee1ff"
 let matches = [
     "0xe0d1977a23cb90252b9997ab07b03136e214e0c6", // stakingUSDC
-    "0x3a85b87e81cd99d4a6670f95a4f0dedaac207da0" // exchanger
+    // "0x3a85b87e81cd99d4a6670f95a4f0dedaac207da0" // exchanger
 ]
 let prefix = "usdc_";
 
